7bf8c61ba522ebe5904edb62313a63e3d33baf7b
mo <rahami@magniuz-MacBook-Pro.local>
2019-11-22 17:40:37 +0800

    finish ui for the pads

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="title-wrapper">
+        <header className="title">JAMQUBE</header>
+        <p className="subtitle">Don't think, just jam.</p>
+      </div>
+
+      <div className="container">
+        <div className="pad1">
+          <div className="padName">Kick</div>
+        </div>
+        <div className="pad2">
+          <div className="padName">Snare</div>
+        </div>
+        <div className="pad3">
+          <div className="padName">Hat</div>
+        </div>
+        <div className="pad4">
+          <div className="padName">Cymbal</div>
+        </div>
+        <div className="pad5">
+          <div className="padName">Shaker</div>
+        </div>
+        <div className="pad6">
+          <div className="padName">bell</div>
+        </div>
+        <div className="pad7">
+          <div className="padName">snap</div>
+        </div>
+        <div className="pad8">
+          <div className="padName">tom</div>
+        </div>
+        <div className="pad9">
+          <div className="padName">Keys1</div>
+        </div>
+        <div className="pad10">
+          <div className="padName">Keys2</div>
+        </div>
+        <div className="pad11">
+          <div className="padName">Keys3</div>
+        </div>
+        <div className="pad12">
+          <div className="padName">Keys4</div>
+        </div>
+      </div>
     </div>
   );
 }
