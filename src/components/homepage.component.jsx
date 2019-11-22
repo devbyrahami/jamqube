5bf23581b4294df30ff4ba43727b28873293a2b7
mo <rahami@magniuz-MacBook-Pro.local>
2019-11-22 21:00:59 +0800

    made a homepage component

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./homepage.styles.css";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="title-wrapper">
+      <header className="title">JAMQUBE</header>
+      <p className="subtitle">Don't think, just jam.</p>
+    </div>
+
+    <div className="container">
+      <div className="pad1">
+        <div className="padName">Kick</div>
+      </div>
+      <div className="pad2">
+        <div className="padName">Snare</div>
+      </div>
+      <div className="pad3">
+        <div className="padName">Hat</div>
+      </div>
+      <div className="pad4">
+        <div className="padName">Cymbal</div>
+      </div>
+      <div className="pad5">
+        <div className="padName">Shaker</div>
+      </div>
+      <div className="pad6">
+        <div className="padName">bell</div>
+      </div>
+      <div className="pad7">
+        <div className="padName">snap</div>
+      </div>
+      <div className="pad8">
+        <div className="padName">tom</div>
+      </div>
+      <div className="pad9">
+        <div className="padName">Keys1</div>
+      </div>
+      <div className="pad10">
+        <div className="padName">Keys2</div>
+      </div>
+      <div className="pad11">
+        <div className="padName">Keys3</div>
+      </div>
+      <div className="pad12">
+        <div className="padName">Keys4</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
